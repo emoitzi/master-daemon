b5299cd9ac23e11eececb6b160e6d6e43761192a
Ewald Moitzi <ewald.moitzi@student.tugraz.at>
2015-02-05 15:23:44 +0100

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # master-daemon
 A node.js module to start scripts as child processes of a daemonized master process using nodes cluster package
+
+
+Usage
+-----
+
+If this process is not started as a child process, childDaemon will call its callback immediately
+### In the child:
+    var childDeamon = require('master-daemon').childDaemon;
+    
+    function codeToRun(error, message, callback) {
+      if(error) {
+        //currently always null 
+      }
+      
+      //only required when startMaster() is called with a callback.
+      if(/*child error*/) {
+        callback(new Error('failed'));
+      }
+      else {
+        callback(null, {some: 'message'});
+      }
+      
+      
+    }
+    
+    childDaemon(codeToRun);
+
+
+This process running this script will exit when master is spawned or when a response from the child is received.
+### In the master:
+    var startMaster = require('master-daemon').startMaster;
+    
+    startMaster({
+      file: 'child.js',
+      instances: 1,        //number of instances to start, 1 if omitted
+      retry: true          //restart instance after stop
+      message: { }         //will be send to child
+    },
+    function (error, message) {    //optional callback. If present this process will
+                                   //wait for a response from the child script.
+    });
---
+++ master-daemon.js
@@ -0,0 +1,95 @@
+/**
+ * Created by emoitzi on 2/3/15.
+ */
+"use strict";
+var spawn   = require('child_process').spawn;
+var fs      = require('fs');
+
+
+/*
+options
+  .file         String      Filename of js file to start
+  .message      Object      Will be send to child instances on start
+  .instances    Number      Number of child instances to start. Default 1
+  .retry        Boolean     If true childDeamon will be restarted on exit.   
+ */
+
+function startMaster(options, callback) {
+    if (!options || !options.file) {
+      throw new Error('ClusterMaster: file parameter required');
+    }
+    if (typeof options.instances !== 'number') {
+      options.instances = 1;
+    }
+    options.cwd = process.cwd();
+
+    var spawn_options = {
+        stdio: ['ignore', 'ignore', 'ignore', 'ipc'],
+        execPath: process.execPath,
+        detached: true
+    };
+
+    var child = spawn(process.execPath, [__dirname + '/lib/master.js'], spawn_options);
+    
+    child.on('error', function (error) {
+        if (typeof callback === 'function') {
+            callback(error);
+        }
+        else {
+            throw error;
+        }
+        
+    });
+    
+    
+    if (typeof callback === 'function') {
+        options.report_success = true;
+        child.on('message', function (message) {
+            fs.writeFileSync('master.log', JSON.stringify(message) + '\n');
+            var error;
+            if (typeof message.error === 'string') {
+                fs.appendFileSync('master.log', 'error is object');
+                error = new Error(message.error);
+            }
+            child.unref();
+            callback(error, message.message);
+        });
+        child.send(options);
+    }
+    else {
+        child.send(options);
+        child.unref();
+        
+    }
+
+};
+
+function childFeedback(error, message) {
+    var feedback = {
+        message: message
+    }
+    if (error instanceof Error) {
+        fs.writeFileSync('child.log', 'Error instance\n');
+        fs.appendFileSync('child.log', error);
+        feedback.error = error.message;
+    }
+    
+    process.send(feedback);
+}
+
+
+function childDaemon(callback) {
+  if (typeof process.send === 'function') {
+    process.on('message', function (message) {
+      callback(null, message.message, childFeedback);
+    });
+  }
+  else {
+    callback(null);
+  }
+}
+
+
+
+exports.startMaster = startMaster;
+exports.childDaemon  = childDaemon;
